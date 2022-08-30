c40de50583902bd856e4c881bb9f49873d66a439
cuong nguyen <vancuong.nguyen@savvycomsoftware.com>
2022-08-30 14:48:53 +0700

    init

---
+++ index.js
@@ -0,0 +1,33 @@
+{
+    "name": "react-native-camera-roll",
+    "version": "1.0.0",
+    "description": "A simple and fully customizable React Native component that implements a progress stepper UI.",
+    "main": "index.js",
+    "scripts": {
+      "test": "echo \"Error: no test specified\" && exit 1"
+    },
+    "repository": {
+      "type": "git",
+      "url": "git+https://github.com/cuongnv2-miichisoft/react-native-camera.git"
+    },
+    "keywords": [
+      "react-native",
+      "react-component",
+      "react-native-component",
+      "react",
+      "react native",
+      "mobile",
+      "ios",
+      "android",
+      "ui",
+      "stepper",
+      "progress",
+      "progress-steps"
+    ],
+    "author": "Colby Miller",
+    "license": "MIT",
+    "bugs": {
+      "url": "https://github.com/cuongnv2-miichisoft/react-native-camera/issues"
+    },
+    "homepage": "https://github.com/cuongnv2-miichisoft/react-native-camera#readme"
+  }
